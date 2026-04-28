b667b5f85208722acbd25c21dc4b85b1c2dc671d
mmrak38-del <mmrak38@gmail.com>
2026-04-28 21:22:09 +0300

    Create Program.cs file

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Windows.Forms;
+namespace TrunkMessenger {
+ static class Program {
+ [STAThread] static void Main() {
+ Application.EnableVisualStyles();
+ Application.Run(new MainForm());
+ }
+ }
+}
